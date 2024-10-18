8ddba467c6ef983acffc2caaf895ba008ac08d37
Rahugg <rahugg07@gmail.com>
2024-10-18 20:52:20 +0500

    basic nuxt setup

--- src/router/index.ts
+++
@@ -1,9 +0,0 @@
-// import { createRouter, createWebHistory } from 'vue-router'
-// import HomeView from '../views/HomeView.vue'
-
-// const router = createRouter({
-//   history:
-//   routes:[]
-// })
-
-// export default router
